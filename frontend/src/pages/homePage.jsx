7cb62ff39f46a3736be55228b95fa8bae1b468ce
AbhinavDileep <abhinavdileep3@gmail.com>
2025-07-28 17:02:09 +0530

    homepage modified

--- frontend/src/pages/homePage.jsx
+++ frontend/src/pages/homePage.jsx
@@ -8,26 +8,6 @@ const homePage = () => {
   return (
     <div>
       <Navbar />
-      {/* <div
-  className="hero min-h-screen"
-  style={{
-    backgroundImage:
-      "url(/c.jpg)",
-  }}
->
-  <div className="hero-overlay"></div>
-  <div className="hero-content text-neutral-content text-center">
-    <div className="max-w-md">
-      <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-      <p className="mb-5">
-        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-        quasi. In deleniti eaque aut repudiandae et a id nisi.
-      </p>
-      <button className="btn btn-primary">Get Started</button>
-    </div>
-  </div>
-</div> */}
-
     <div
       className="h-[40rem] rounded-md bg-neutral-900 flex flex-col items-center justify-center relative w-full">
       <h2
@@ -37,7 +17,7 @@ const homePage = () => {
         <span>Star Background</span>
       </h2>
       <br></br>
-      <button className="btn btn-outline btn-primary">Get Started</button>
+      <button className="btn z-10 btn-outline btn-primary">Get Started</button>
       <ShootingStars />
       <StarsBackground />
     </div>
