65a1c409ca4930ecff98c505c327ee14775fac31
AbhinavDileep <abhinavdileep3@gmail.com>
2025-09-22 19:08:06 +0530

    modified about page

--- frontend/src/pages/about.jsx
+++ frontend/src/pages/about.jsx
@@ -39,7 +39,7 @@ const AboutPage = () => {
         <div className="flex flex-col items-center justify-center h-[40rem] max-w-3xl mx-auto space-y-6">
           
           <p className="text-neutral-600 dark:text-neutral-200 text-xs sm:text-white">
-            About Local Aid Management System
+           <h2> About Local Aid Management System </h2>
           </p>
 
           <TypewriterEffectSmooth words={words} />
@@ -51,11 +51,7 @@ const AboutPage = () => {
             LAMS ensures that you can connect quickly, securely, and conveniently — all in one place.
           </p>
 
-          <p className="text-neutral-300 dark:text-neutral-100 text-sm sm:text-base leading-relaxed mt-4">
-            With features like <span className="font-medium">real-time availability, secure payments, 
-            AI chatbot support, and verified credentials</span>, our goal is to simplify access 
-            to essential services while ensuring trust and reliability within communities.
-          </p>
+        
 
           <p className="text-neutral-400 dark:text-neutral-200 italic mt-6">
             "LAMS — bringing local help closer to you, one click at a time."
